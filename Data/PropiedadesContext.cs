38b9e3a1ebc70f18d5670bfb1bb7028c021e322c
JAUN DAVID HERNANDEZ PERDOMO <juandavidhernandez515@hotmail.com>
2024-11-19 22:11:56 -0500

    Migracion Db

---
+++ Data/PropiedadesContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PropiedadesWEB.Models;
+using static System.Net.Mime.MediaTypeNames;
+using System.Diagnostics.Metrics;
+using System;
+using PropiedadWEB.Models;
+
+namespace PropiedadesWEB.Data
+{
+	public class PropiedadesContext : DbContext
+	{
+		public DbSet<Propiedad> Propiedads { get; set; }
+		public DbSet<Inquilino> Inquilinos { get; set; }
+		public DbSet<Contrato> Contratos { get; set; }
+		public DbSet<Pago> Pagos { get; set; }
+		public DbSet<User> User { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(
+				"Server=(localdb)\\mssqllocaldb;Database=PropertyManagerDB;Trusted_Connection=True;");
+		}
+	}
+
+	
+}
